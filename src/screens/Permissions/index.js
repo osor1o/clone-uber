cabbcd7310a37b093b45c7d0387815742028e9c6
osor1o <osorioneto97@gmail.com>
2020-11-10 00:39:54 -0300

    create permissions screen

---
+++ src/screens/Permissions/index.js
@@ -0,0 +1,41 @@
+import React, {useEffect, useCallback} from 'react';
+import {View, PermissionsAndroid, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import Button from '../../components/Button';
+import styles from './styles';
+
+export default () => {
+  const navigation = useNavigation();
+
+  const requestPermissions = useCallback(async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      navigation.reset({
+        index: 0,
+        routes: [
+          {
+            name: 'Map',
+          },
+        ],
+      });
+    }
+  }, [navigation]);
+
+  useEffect(() => {
+    requestPermissions();
+  }, [requestPermissions]);
+
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.text}>
+          Para utilizar o aplicativo é necessário liberar as permissões
+        </Text>
+        <Button onPress={requestPermissions}>Tentar Novamente</Button>
+      </View>
+    </View>
+  );
+};
---
+++ src/screens/Permissions/styles.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+  },
+  content: {},
+  text: {
+    fontSize: 24,
+    paddingBottom: 24,
+  },
+});
